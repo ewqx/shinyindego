99af6edbe5b99e1e8f34fe699b4e8905d6e987f0
“ewqx” <ecn2318@gmail.com>
2021-04-03 23:41:58 -0400

    parse date/time column

--- datacleaning.R
+++ datacleaning.R
@@ -1,25 +1,111 @@
+## TITLE HERE
+
 # CLEAR ENVIRONMENT
 rm(list = ls())
 
 # LOAD LIBRARIES
 library(here)
 library(dplyr)
+library(tidyr) 
 library(ggplot2)
+library(lubridate)
+library(data.table)  
 
 # DEFINE DIRECTORY 
 # https://github.com/jennybc/here_here
 cwd <- here::here()
+cwd
 
 # DEFINE SUBDIRECTORIES
-subdir <- "Documents/dsa/Mod04DataAnalysisR/indego"
-data_subdir <- "data"
+subdir <- "/Users/estheroids/Documents/dsa/Mod04DataAnalysisR/12-R_ShinyProj/shinyindego"
+#data_subdir <- "data"
 
 # SET DIRECTORY
 setwd(here(subdir))
 getwd()
 
-csv_filenames <- list.files('./data', pattern = 'csv')
-csv_filepaths <- paste0( './data/', csv_filenames, sep = '' )
+# LOAD DATA
+### Indego Data ###
+# Data source: https://www.rideindego.com/about/data/
+'Indego, Philadelphia’s bike-share program, offers 24-hour, 30-day, and yearly pass options to ride one of the hundreds of bicycles stationed at more than 140 stations around the city.' 
 
-all_dfs <- lapply( csv_filepaths, FUN = function( fp ) read.csv( fp, stringsAsFactors = F ) )
+#2018, 2019, 2020
+#Q1 (January – March)
+#Q2 (April – June)
+#Q3 (July – September)s
+#Q4 (October – December)
+
+#make a character vector of all csv file names in the ./data directory
+csv_fnames <- list.files('./data', pattern = 'csv')
+
+#create character vector appending ./data/ to each entry of csv_fnames so each one would be the full file path
+csv_fpaths <- paste0('./data/', csv_fnames, sep = '')
+
+#read each of the file paths in the vector into a dataframe object, storing all the data frames into a single list
+list_dfs <- lapply(csv_fpaths, fread)
+#all_dfs <- lapply( csv_fpaths, FUN = function( fp ) read.csv( fp, stringsAsFactors = F ) )
+
+head(list_dfs)
+colnames(list_dfs[[5]])
+ncol(list_dfs[[5]]) #15
+nrow (list_dfs[[5]])
+head (list_dfs[[5]]) #2019 Q1
+lapply(list_dfs[[5]], class)
+lapply(list_dfs[[7]], class)
+
+#$trip_id [1] "integer"
+#$duration [1] "integer"
+#$start_time [1] "POSIXct" "POSIXt" [1] "character"
+#$end_time [1] "POSIXct" "POSIXt" [1] "character"
+#$start_station [1] "integer"
+#$start_lat [1] "numeric"
+#$start_lon [1] "numeric"
+#$end_station [1] "integer"
+#$end_lat [1] "numeric"
+#$end_lon [1] "numeric"
+#$bike_id [1] "integer"
+#$plan_duration [1] "integer"
+#$trip_route_category [1] "character"
+#$passholder_type [1] "character"
+#$bike_type [1] "character"
+#$start_date [1] "Date"
+
+list_dfs[6] #2019-04-01 00:01:00 
+list_dfs[7] #7/1/2019 0:01
+
+#create date column - extract year from start_time
+clean_dt <- function(dt){
+  require(lubridate)
+  parse_date_time (dt, orders = c('YmdHMS','mdYHM') )
+}
+
+fmt_df_dt <- function(x) 
+  x %>% mutate_at(vars(start_time, end_time), clean_dt)
+
+list_dfs <- lapply(list_dfs, fmt_df_dt)
+
+add_datecol <- function(df)
+  df %>% mutate(
+    start_date = as.Date(start_time))
+
+list_dfs <- lapply(list_dfs, add_datecol)
+
+head(list_dfs)
+list_dfs[6] 
+list_dfs[10]
+
+#create year and quarter column
+
+add_yearcol <- function(df)
+  df %>% mutate(
+    start_year = format(start_time, format="%Y"))
+
+list_dfs <- lapply(list_dfs, add_yearcol)
+
+
+
+
+
+
+
 
